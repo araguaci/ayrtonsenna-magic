f40b82d76a5b1f8807fa5a7f109e14433c23056c
araguaci <araguaci@gmail.com>
2024-04-29 14:38:11 -0300

    card:https://ayrtonsenna-magic.vercel.app/

--- .history/astro.config_20240330003446.mjs
+++ .history/astro.config_20240330003446.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ayrtonsenna-magic.vercel.app/',
+  site: 'https://astro-multiverse.vercel.app',
 })
